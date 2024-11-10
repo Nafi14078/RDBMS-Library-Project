68371e9729d0a93b530a3a9d5968d730e8f452ee
Ashfak Azad Nafi <ashfakazadnafi@gmail.com>
2024-11-10 19:38:06 +0600

    seventh commit

---
+++ php/func-author.php
@@ -0,0 +1,16 @@
+<?php
+#Get all Author  function
+
+function get_all_author($con){
+    $sql = "SELECT*FROM authors ";
+    $stmt =$con->prepare($sql);
+    $stmt->execute();
+
+    if($stmt->rowCount()> 0){
+        $authors =$stmt->fetchAll();
+    }else{
+        $authors = 0;
+    }
+    return $authors;
+}
+
